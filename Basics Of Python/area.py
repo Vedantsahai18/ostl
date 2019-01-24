5d7572e76233dbac591a4245b57664863ae0223e
Shubham Pednekar <32964043+txtr@users.noreply.github.com>
2019-01-24 21:17:33 +0530

    area.py added
    Signed-off-by: Shubham Pednekar <32964043+txtr@users.noreply.github.com>

---
+++ Basics Of Python/area.py
@@ -0,0 +1,44 @@
+import math as m
+
+
+class Shape:
+    # constructor
+    def __init__(self, dimensions):
+        self.dimension = list(dimensions)
+        self.name = self.get_name()
+        self.area = self.get_area()
+
+    def get_name(self):
+        if len(self.dimension) == 4:
+            if len(set(self.dimension)) == 1:
+                return "square"
+            if len(set(self.dimension)) == 2:
+                return "rectangle"
+            else:
+                return None
+        elif len(self.dimension) == 3:
+            return "triangle"
+
+    def get_area(self):
+        if self.name == "rectangle":
+            area = 1
+            for side in set(self.dimension):
+                area *= side
+            return area
+        elif self.name == "square":
+            area = (self.dimension[0]) * (self.dimension[0])
+            return area
+        elif self.name == "triangle":
+            a = self.dimension[0]
+            b = self.dimension[1]
+            c = self.dimension[2]
+            s = (a + b + c) / 2
+            # using heron's formula for calculating area of triangle
+            area = m.sqrt(s * (s - a) * (s - b) * (s - c))
+            return area
+
+
+if __name__ == '__main__':
+    dimensions = float(input("Enter measurements of all sides of the shape : ").split(" "))
+    shape = Shape(dimensions)
+    print("The shape is a ", shape.name, " with an area of ", shape.area)
