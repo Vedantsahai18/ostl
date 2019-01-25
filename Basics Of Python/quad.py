7a51c84f1f77fc6f5d9f334701ae0d8a60899217
Shubham Pednekar <32964043+txtr@users.noreply.github.com>
2019-01-25 20:58:27 +0530

    quad.py added
    Signed-off-by: Shubham Pednekar <32964043+txtr@users.noreply.github.com>

--- Basics Of Python/quad.py
+++ Basics Of Python/quad.py
@@ -1,8 +1,9 @@
 import cmath as cm
 
-a = int(input("Enter coefficient of x^2 :"))
-b = int(input("Enter coefficient of x   :"))
-c = int(input("Enter the constant term  :"))
+if __name__ == '__main__':
+    a = int(input("Enter coefficient of x^2 :"))
+    b = int(input("Enter coefficient of x   :"))
+    c = int(input("Enter the constant term  :"))
 
-print ("The solution of ", a, " x^2 + ", b, " x + ", c, " are ", ((-1) * b + cm.sqrt(b * b - 4 * a * c)) / (2 * a),
-       "and", ((-1) * b + cm.sqrt(b * b - 4 * a * c)) / (2 * a))
+    print("The solution of ", a, " x^2 + ", b, " x + ", c, " are ", ((-1) * b + cm.sqrt(b * b - 4 * a * c)) / (2 * a),
+          "and", ((-1) * b + cm.sqrt(b * b - 4 * a * c)) / (2 * a))
